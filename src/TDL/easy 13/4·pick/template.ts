92435fb80ede748ff675afd603e33d59d49fa61f
sleepyheadp <17863965581@163.com>
2024-02-21 15:34:41 +0800

    easy 4 · pick

--- src/TDL/easy 13/4·pick/template.ts
+++ src/TDL/easy 13/4·pick/template.ts
@@ -1,1 +1,15 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+	// 遍历K，将T中的K属性取出来, 生成新的对象
+	[P in K]: T[P];
+};
+
+// js实现
+function myPick(T, K) {
+	let picked = {};
+	K.forEach((key) => {
+		if (key in T) {
+			picked[key] = T[key];
+		}
+	});
+	return picked;
+}
